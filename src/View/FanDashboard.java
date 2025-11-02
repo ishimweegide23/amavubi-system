c47a4f50f1f099b179e1cb9ce060f25734e8455f
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-02 13:30:00 +0200

    feat: implement RMI server (RMIServer.java)

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -341,3 +341,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #18 - 11/02/2025 02:05:50
 // Amavubi auto-commit #19 - 11/02/2025 02:05:50
 // Amavubi auto-commit #20 - 11/02/2025 02:05:51
+// Amavubi auto-commit #21 - 11/02/2025 02:05:51
