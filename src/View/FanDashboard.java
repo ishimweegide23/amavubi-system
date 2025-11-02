470453796d52d375b7c97ffc7d7b59b970c2a90d
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-02 18:30:00 +0200

    feat: enable remote fan registration via RMI

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -343,3 +343,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #20 - 11/02/2025 02:05:51
 // Amavubi auto-commit #21 - 11/02/2025 02:05:51
 // Amavubi auto-commit #22 - 11/02/2025 02:05:52
+// Amavubi auto-commit #23 - 11/02/2025 02:05:53
