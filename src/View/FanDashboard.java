337feb34b38018407c4abb8c0dd079b671738bae
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-04 13:00:00 +0200

    feat: display vote results with live chart

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -360,3 +360,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #37 - 11/02/2025 02:06:01
 // Amavubi auto-commit #38 - 11/02/2025 02:06:01
 // Amavubi auto-commit #39 - 11/02/2025 02:06:01
+// Amavubi auto-commit #40 - 11/02/2025 02:06:02
