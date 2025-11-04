546b8c6e587e4339d7b1d6ffe8411f5618b2e56e
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-04 08:00:00 +0200

    feat: implement fan search with autocomplete

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -358,3 +358,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #35 - 11/02/2025 02:05:59
 // Amavubi auto-commit #36 - 11/02/2025 02:06:00
 // Amavubi auto-commit #37 - 11/02/2025 02:06:01
+// Amavubi auto-commit #38 - 11/02/2025 02:06:01
