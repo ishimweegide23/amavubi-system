6c4f7b0c15e9650a3ae42e6f89672b961d237bd2
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-06 00:00:00 +0200

    feat: add season ticket (10 matches discount)

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -374,3 +374,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #51 - 11/02/2025 02:06:08
 // Amavubi auto-commit #52 - 11/02/2025 02:06:09
 // Amavubi auto-commit #53 - 11/02/2025 02:06:09
+// Amavubi auto-commit #54 - 11/02/2025 02:06:10
