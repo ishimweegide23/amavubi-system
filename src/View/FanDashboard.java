20778fc0eb128e32d9120068589abf8196e9c2dd
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-06 22:30:00 +0200

    feat: award 50 points for correct score

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -383,3 +383,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #60 - 11/02/2025 02:06:13
 // Amavubi auto-commit #61 - 11/02/2025 02:06:13
 // Amavubi auto-commit #62 - 11/02/2025 02:06:14
+// Amavubi auto-commit #63 - 11/02/2025 02:06:15
