0792ae6653b35b3b7844729e0cf54b708bfc45f4
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-06 20:00:00 +0200

    feat: add match prediction game

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -382,3 +382,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #59 - 11/02/2025 02:06:12
 // Amavubi auto-commit #60 - 11/02/2025 02:06:13
 // Amavubi auto-commit #61 - 11/02/2025 02:06:13
+// Amavubi auto-commit #62 - 11/02/2025 02:06:14
