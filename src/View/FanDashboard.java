07fc64c0f8de660edea3461e041ee2e465555012
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-07 16:00:00 +0200

    feat: build executable JAR with launch4j

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -390,3 +390,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #67 - 11/02/2025 02:06:17
 // Amavubi auto-commit #68 - 11/02/2025 02:06:18
 // Amavubi auto-commit #69 - 11/02/2025 02:06:18
+// Amavubi auto-commit #70 - 11/02/2025 02:06:19
