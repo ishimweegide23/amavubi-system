5b063bd99b11830495f7c398588d5fdc65575688
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-10 01:30:00 +0200

    docs: add RMI interface documentation

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -413,3 +413,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #90 - 11/02/2025 02:06:29
 // Amavubi auto-commit #91 - 11/02/2025 02:06:30
 // Amavubi auto-commit #92 - 11/02/2025 02:06:30
+// Amavubi auto-commit #93 - 11/02/2025 02:06:31
