cef5a9bf82d8f3851ef24ffc06fb5a6276133193
ishimweegide23 <ishimweegide20@gmail.com>
2025-11-09 08:00:00 +0200

    feat: integrate Google Maps (mock)

--- src/View/FanDashboard.java
+++ src/View/FanDashboard.java
@@ -406,3 +406,4 @@ this.dispose(); // This line prevents coming back!
 // Amavubi auto-commit #83 - 11/02/2025 02:06:25
 // Amavubi auto-commit #84 - 11/02/2025 02:06:26
 // Amavubi auto-commit #85 - 11/02/2025 02:06:26
+// Amavubi auto-commit #86 - 11/02/2025 02:06:27
